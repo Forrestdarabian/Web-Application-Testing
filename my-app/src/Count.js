5c7c776c07ae14c8414fe522505d57b6d954a964
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-08-09 07:35:11 -0700

    MVP

--- my-app/src/Count.js
+++ my-app/src/Count.js
@@ -1,8 +1,26 @@
-import { render } from 'react-testing-library'
-export const strike = (currState) => {
-  if(currState + 1 >= 3) {
-    return 0
-  } else {
-    return currState + 1
-  };
-};
+import React from 'react';
+import { render } from '@testing-library/react';
+import Display from './Display.js';
+
+
+describe('<Display />', () => {
+  it('renders without crashing', () => {
+    render(<Display />);
+  });
+  it('props work', () => {
+    const balls = 3, strikes = 2;
+    const display = render(<Display strikes={strikes} balls={balls} />);
+    display.getByText(new RegExp(`Strikes: ${strikes}`, 'i'));
+    display.getByText(new RegExp(`Balls: ${balls}`, 'i'));
+  });
+  it('props work', () => {
+    const display = render(<Display strikes={1} balls={2} />);
+    display.getByText(/Strikes: 1/);
+    display.getByText(/Balls: 2/);
+  });
+  it('Component is dumb', () => {
+    const display = render(<Display strikes={-2} balls={400} />);
+    display.getByText(/Strikes: -2/);
+    display.getByText(/Balls: 400/);
+  });
+});
